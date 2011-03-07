f98d42326dd4e2b8cad85c52c97ee3214bd4ad48
my.mail.jonatas <my.mail.jonatas@gmail.com>
2011-03-07 23:47:49 +0000

    small updates

--- init.js
+++ init.js
@@ -38,6 +38,7 @@ theWebUI.redrawFilesRelocate = function(hash)
 				var file = this.files[hash][i];
 				file.percent = (file.size > 0) ? theConverter.round((file.done/file.size)*100,1): "100.0";
 				file.relocate = "bla";
+				console.debug(file);
 				if(this.settings["webui.fls.view"])
 				{
 				if(!$type(table.rowdata[sId])) 
