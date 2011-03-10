adf2ea0d74c6b368512cd60e7d20fb5c895c66c2
my.mail.jonatas@gmail.com <my.mail.jonatas@gmail.com@f47358ce-8a60-5318-5bf3-d0e4e61b668a>
2011-03-10 11:52:18 +0000

    small changes

--- trunk/init.js
+++ trunk/init.js
@@ -49,7 +49,7 @@ plugin.onLangLoaded = function()
 			"<fieldset>" +
 				"<label id='lbl_relocate' for='edit_relocate'>" + theUILang.Relocate + ": </label>" +
 				"<input type='file' name='edit_relocate' id='edit_relocate' class='TextboxLarge' size='42'>"+
-				"/*<input type='button' id='btn_relocate_browse' class='Button' value='...' />*/" +
+				"<!--<input type='button' id='btn_relocate_browse' class='Button' value='...' />-->" +
 			"</fieldset>" +
 		"</div>"+
 		"<div class='aright buttons-list'>" +
