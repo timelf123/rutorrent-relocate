8bdc316f8ac1770d459c01f86738bd9a4ed449cf
my.mail.jonatas@gmail.com <my.mail.jonatas@gmail.com@f47358ce-8a60-5318-5bf3-d0e4e61b668a>
2011-03-18 11:18:52 +0000

    tagging version 0.1

---
+++ tags/0.1/action.php
@@ -0,0 +1,66 @@
+<?php
+/*require_once( '../../php/xmlrpc.php' );
+
+eval( getPluginConf( 'relocate' ) );
+
+echo $_REQUEST['relocate'];
+
+function relocate() {
+	if(isset($_REQUEST['hash']) && isset($_REQUEST['no'])) {
+    	$req = new rXMLRPCRequest( 
+			new rXMLRPCCommand( "f.get_frozen_path", array($_REQUEST['hash'],intval($_REQUEST['no']))) );
+	if($req->success())
+	{
+		$filename = $req->val[0];
+		if($filename=='')
+		{
+			$req = new rXMLRPCRequest( array(
+				new rXMLRPCCommand( "d.open", $_REQUEST['hash'] ),
+				new rXMLRPCCommand( "f.get_frozen_path", array($_REQUEST['hash'],intval($_REQUEST['no'])) ),
+				new rXMLRPCCommand( "d.close", $_REQUEST['hash'] ) ) );
+			if($req->success())
+				$filename = $req->val[1];
+		}
+        //return(shell_exec(getExternal("mediainfo").' --Output=HTML "'.$filename.'"'));
+      }
+	}
+return false;
+}
+echo relocate();*/
+	
+function escape($v){
+	
+	$v = str_replace(" ","\ ",$v);
+	$v = str_replace("'","\'",$v);
+	return $v;
+	
+}
+
+$dest =  escape($_REQUEST['dest']);
+$source =  escape($_REQUEST['sourc']);
+
+if ($_REQUEST['force'] == 'on') {
+	
+	$args =  'f';
+	
+}else{
+	
+	$args =  '';
+	
+}
+
+function relocate($source, $dest, $args) {
+	
+	if(isset($source) && isset($dest)) {
+		
+		return(shell_exec("ln -s".$args." '".$dest."' '".$source."'"));
+		//echo ("ln -s".$args." '".$dest."' '".$source."'");
+		
+	}
+	
+	return false;
+}
+
+echo relocate($source, $dest, $args);
+	
+?>
---
+++ tags/0.1/lang/en.js
@@ -0,0 +1,9 @@
+﻿theUILang.RelocateMenuCaption		= "Relocate...";
+theUILang.RelocateDlgCaption		= "Relocate Files";
+theUILang.RelocateFrmDestination	= "Destination";
+theUILang.RelocateFrmSoure			= "Source";
+theUILang.RelocateFrmOptions		= "Options";
+theUILang.RelocateFrmDebug			= "Debug";
+theUILang.RelocateEmptyDest			= "ERROR: Destination File Empty";
+
+thePlugins.get("relocate").langLoaded();
