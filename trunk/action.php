96baaa51bc06b50fb6905ec6c1001738619a6632
my.mail.jonatas@gmail.com <my.mail.jonatas@gmail.com@f47358ce-8a60-5318-5bf3-d0e4e61b668a>
2011-03-25 15:47:14 +0000

    Small Changes

--- trunk/action.php
+++ trunk/action.php
@@ -1,32 +1,4 @@
 <?php
-/*require_once( '../../php/xmlrpc.php' );
-
-eval( getPluginConf( 'relocate' ) );
-
-echo $_REQUEST['relocate'];
-
-function relocate() {
-	if(isset($_REQUEST['hash']) && isset($_REQUEST['no'])) {
-    	$req = new rXMLRPCRequest( 
-			new rXMLRPCCommand( "f.get_frozen_path", array($_REQUEST['hash'],intval($_REQUEST['no']))) );
-	if($req->success())
-	{
-		$filename = $req->val[0];
-		if($filename=='')
-		{
-			$req = new rXMLRPCRequest( array(
-				new rXMLRPCCommand( "d.open", $_REQUEST['hash'] ),
-				new rXMLRPCCommand( "f.get_frozen_path", array($_REQUEST['hash'],intval($_REQUEST['no'])) ),
-				new rXMLRPCCommand( "d.close", $_REQUEST['hash'] ) ) );
-			if($req->success())
-				$filename = $req->val[1];
-		}
-        //return(shell_exec(getExternal("mediainfo").' --Output=HTML "'.$filename.'"'));
-      }
-	}
-return false;
-}
-echo relocate();*/
 	
 function escape($v){
 	
@@ -39,7 +11,7 @@ function escape($v){
 $dest =  escape($_REQUEST['dest']);
 $source =  escape($_REQUEST['sourc']);
 
-if ($_REQUEST['force'] == 'on') {
+if ($_REQUEST['force']) {
 	
 	$args =  'f';
 	
@@ -53,8 +25,8 @@ function relocate($source, $dest, $args) {
 	
 	if(isset($source) && isset($dest)) {
 		
-		return(shell_exec("ln -s".$args." '".$dest."' '".$source."'"));
-		echo ("Success: ln -s".$args." '".$dest."' '".$source."'");
+		echo ("Success: ln -s".$args." ".$dest." ".$source."");
+		return(shell_exec("ln -s".$args." ".$dest." ".$source.""));
 		
 	}
 	
